afacc21264cfb8bfb66d2d60e92f8e887b79c8b8
Herokroos <shangceh.su@gmail.com>
2023-09-15 21:27:45 +0800

    2023/9/15

---
+++ Core/Src/main.c
@@ -0,0 +1,577 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "fatfs.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "fatfs_sd_card.h"
+#include <HX711.h>
+#include "string.h"
+#include "stdio.h"
+#include "stdbool.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+FATFS       FatFs;                //Fatfs handle
+FIL         fil;                  //File handle
+FRESULT     fres;                 //Result after operations
+char        buf[100];
+char time[10];
+char data[50];
+float Thrust;
+hx711_t loadcell;
+int Start_button_state = 1;
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi1;
+
+TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim4;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+/* hx711 calibrate here*/
+void init_weight(hx711_t *hx711){
+	char buffer[128] = {0};
+	sprintf(buffer,"HX711 initialization\n\r");
+	HAL_UART_Transmit(&huart2, (uint8_t *)(buffer), sizeof(buffer), 100);
+
+	/* Initialize the hx711 sensors */
+	hx711_init(hx711, PD_SCK_GPIO_Port, PD_SCK_Pin, DAT_GPIO_Port, DAT_Pin);
+
+	/* Configure gain for each channel (see datasheet for details) */
+	set_gain(hx711, 128, 32);
+
+	/* Set HX711 scaling factor (see README for procedure) */
+	set_scale(hx711, 206.4, 1);
+
+	/* Tare weight */
+	tare_all(hx711, 10);
+
+	sprintf(buffer,"HX711 module has been initialized\n\r");
+	HAL_UART_Transmit(&huart2, (uint8_t *)(buffer), sizeof(buffer), 100);
+}
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_SPI1_Init(void);
+static void MX_TIM2_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_TIM4_Init(void);
+/* USER CODE BEGIN PFP */
+void process_SD_card( void );
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* delay function*/
+void delay2_ms(uint32_t target) {
+	__HAL_TIM_SET_COUNTER(&htim2, 0);
+	HAL_TIM_Base_Start(&htim2);
+	while(__HAL_TIM_GET_COUNTER(&htim2) < target) {
+		// waiting counter to count to target
+	    }
+}
+void delay4_ms(uint32_t target) {
+	__HAL_TIM_SET_COUNTER(&htim4, 0);
+	HAL_TIM_Base_Start(&htim4);
+	while(__HAL_TIM_GET_COUNTER(&htim4) < target) {
+		// waiting counter to count to target
+	    }
+}
+
+/* sequence initialized function to count down*/
+void SI_func(){
+	for (int b =0;b<= 10;b++ ){
+			HAL_GPIO_WritePin(Buzzer_GPIO_Port, Buzzer_Pin, GPIO_PIN_SET);
+			delay2_ms(500);
+			HAL_GPIO_WritePin(Buzzer_GPIO_Port, Buzzer_Pin, GPIO_PIN_RESET);
+			delay2_ms(500);
+		}
+	HAL_GPIO_WritePin(Buzzer_GPIO_Port, Buzzer_Pin, GPIO_PIN_RESET);
+}
+void ignition(){
+	HAL_GPIO_WritePin(ignition_GPIO_Port, ignition_Pin, GPIO_PIN_SET);
+	delay2_ms(500);
+	HAL_GPIO_WritePin(ignition_GPIO_Port, ignition_Pin, GPIO_PIN_RESET);
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SPI1_Init();
+  MX_TIM2_Init();
+  MX_USART2_UART_Init();
+  MX_FATFS_Init();
+  MX_TIM4_Init();
+  /* USER CODE BEGIN 2 */
+  init_weight(&loadcell);
+  process_SD_card();
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1){
+  	  /* USER CODE END WHILE */
+  /* motor insert */
+	  Start_button_state = HAL_GPIO_ReadPin(SWpin_GPIO_Port, SWpin_Pin);
+  	  if(Start_button_state == GPIO_PIN_RESET){
+  		  tare_all(&loadcell, 10);
+  		  delay2_ms(1000);
+  /* to indicate that it is tared */
+  		  for(int u =0;u <= 5;u++ ){
+  			  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
+  			  delay2_ms(500);
+  			  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+  			  delay2_ms(500);
+  	       	  }
+  		  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+  		  delay4_ms(5000);
+  /* SD card */
+  		  f_mount(&FatFs, "", 1);
+  		  f_open(&fil, "Thrust.txt", FA_WRITE | FA_READ | FA_CREATE_ALWAYS);
+  /* count down function called and set the timer*/
+  		  HAL_TIM_Base_Start(&htim4);
+  		  __HAL_TIM_SET_COUNTER(&htim4, 0);
+  		  SI_func();
+  		  delay2_ms(1000);
+  		  ignition();
+  /* Read data from the load cell at T-0 and send to USART and SD card*/
+  		  while(__HAL_TIM_GET_COUNTER(&htim4) < 10000){
+  		  		  Thrust = get_weight(&loadcell, 1, CHANNEL_A);
+  		  		  printf("Thrust = %.3f\n", Thrust);
+  		  		  snprintf(data, sizeof(data), "%.3f\n", Thrust);
+  		  		  UINT bytesWritten;
+  		  		  f_write(&fil, data, strlen(data), &bytesWritten);
+  		  }
+  		HAL_TIM_Base_Stop(&htim4);
+  /* data stop writing */
+  		  f_close(&fil);
+  		  f_mount(NULL, "", 0);
+  		  delay2_ms(5000);
+  /* sequence terminated */
+  		  HAL_GPIO_WritePin(Buzzer_GPIO_Port, Buzzer_Pin, GPIO_PIN_SET);// high level
+  		  delay2_ms(500);
+  		  HAL_GPIO_WritePin(Buzzer_GPIO_Port, Buzzer_Pin, GPIO_PIN_RESET);// low level
+  	  }else{
+  		  Thrust = get_weight(&loadcell, 10, CHANNEL_A);
+  		  printf("Thrust = %.3f\n", Thrust);
+  	  }
+      /* USER CODE BEGIN 3 */
+        }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_4;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 8000;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 10000;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+
+}
+
+/**
+  * @brief TIM4 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM4_Init(void)
+{
+
+  /* USER CODE BEGIN TIM4_Init 0 */
+
+  /* USER CODE END TIM4_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM4_Init 1 */
+
+  /* USER CODE END TIM4_Init 1 */
+  htim4.Instance = TIM4;
+  htim4.Init.Prescaler = 8000;
+  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim4.Init.Period = 30000;
+  htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM4_Init 2 */
+
+  /* USER CODE END TIM4_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LED_Pin|CS_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, ignition_Pin|Buzzer_Pin|PD_SCK_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LED_Pin CS_Pin */
+  GPIO_InitStruct.Pin = LED_Pin|CS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SWpin_Pin Tare_Pin DAT_Pin */
+  GPIO_InitStruct.Pin = SWpin_Pin|Tare_Pin|DAT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : ignition_Pin Buzzer_Pin PD_SCK_Pin */
+  GPIO_InitStruct.Pin = ignition_Pin|Buzzer_Pin|PD_SCK_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+int __io_putchar(int ch) {
+    ITM_SendChar(ch);
+    return ch;
+}
+void process_SD_card( void )
+{
+  FATFS       FatFs;                //Fatfs handle
+  FIL         fil;                  //File handle
+  FRESULT     fres;                 //Result after operations
+  char        buf[100];
+
+  do
+  {
+    //Mount the SD Card
+    fres = f_mount(&FatFs, "", 1);    //1=mount now
+    if (fres != FR_OK)
+    {
+      printf("No SD Card found : (%i)\r\n", fres);
+      break;
+    }
+    printf("SD Card Mounted Successfully!!!\r\n");
+
+    //Read the SD Card Total size and Free Size
+    FATFS *pfs;
+    DWORD fre_clust;
+    uint32_t totalSpace, freeSpace;
+
+    f_getfree("", &fre_clust, &pfs);
+    totalSpace = (uint32_t)((pfs->n_fatent - 2) * pfs->csize * 0.5);
+    freeSpace = (uint32_t)(fre_clust * pfs->csize * 0.5);
+
+    printf("TotalSpace : %lu bytes, FreeSpace = %lu bytes\n", totalSpace, freeSpace);
+
+    //Open the file
+    fres = f_open(&fil, "test.txt", FA_WRITE | FA_READ | FA_CREATE_ALWAYS);
+    if(fres != FR_OK)
+    {
+      printf("File creation/open Error : (%i)\r\n", fres);
+      break;
+    }
+
+    printf("Writing data!!!\r\n");
+    //write the data
+    f_puts("Ready to conduct", &fil);
+
+    //close your file
+    f_close(&fil);
+
+    //Open the file
+    fres = f_open(&fil, "test.txt", FA_READ);
+    if(fres != FR_OK)
+    {
+      printf("File opening Error : (%i)\r\n", fres);
+      break;
+    }
+
+    //read the data
+    f_gets(buf, sizeof(buf), &fil);
+
+    printf("Read Data : %s\n", buf);
+
+    //close your file
+    f_close(&fil);
+    printf("Closing File!!!\r\n");
+#if 0
+    //Delete the file.
+    fres = f_unlink(test.txt);
+    if (fres != FR_OK)
+    {
+      printf("Cannot able to delete the file\n");
+    }
+#endif
+  } while( false );
+
+  //We're done, so de-mount the drive
+  f_mount(NULL, "", 0);
+  printf("SD Card Unmounted Successfully!!!\r\n");
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
